caf84cd2663bc16d990e58cf3e3fe7f0e1a02e43
vaelntino:manzon <valentinomanzon@gmail.com>
2022-10-04 09:35:49 +0200

    add all

--- web/views.py
+++ web/views.py
@@ -12,6 +12,11 @@ def farms_list(request):
     farms = Farm.objects.all()
     return HttpResponse(template.render({'farms': farms}, request))
 
+def farm_detail(request):
+    template = loader.get_template('web/farms.html')
+    farms = Farm.objects.all()
+    return HttpResponse(template.render({'farms': farms}, request))
+
 def products_list(request):
     template = loader.get_template('web/products.html')
     products = Product.objects.all()
