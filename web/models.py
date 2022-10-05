75a8a47e74e0d0fcbf5fde9d7f64b91f7bf8494c
vaelntino:manzon <valentinomanzon@gmail.com>
2022-10-05 09:21:44 +0200

    add map

--- web/models.py
+++ web/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=30)
     price = models.FloatField()
-    image = models.URLField()
+    image = models.CharField(max_length=200)
     is_per_unit = models.BooleanField(default=False)
 
 class Farm(models.Model):
     name = models.CharField(max_length=30)
-    image = models.URLField()
+    image = models.CharField(max_length=200)
     products = models.ManyToManyField(Product)
     city = models.CharField(max_length=30)
     street = models.CharField(max_length=30)
